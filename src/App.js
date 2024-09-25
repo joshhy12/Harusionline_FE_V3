d47752a50335d53d920251243f8a7bb09d3dda05
joshy Lucas <96329740+joshhy12@users.noreply.github.com>
2024-09-25 13:16:38 +0300

    adding new footer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './App.css';
 import Footer from './components/Footer';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
   return (
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,20 +1,48 @@
 import React from 'react';
-import './styles/Footer.css';
+import logo from './images/logo.png';
+import { Container, Row, Col, Nav } from 'react-bootstrap';
+import { FaFacebook, FaTwitter, FaInstagram, FaLinkedin, FaApple, FaAndroid } from 'react-icons/fa';
 
 const Footer = () => {
   return (
-    <footer className="footer">
-      <div className="footer-content">
-        <p>&copy; 2023 Your Company Name. All rights reserved.</p>
-        <nav>
-          <ul>
-            <li><a href="#">Home</a></li>
-            <li><a href="#">About</a></li>
-            <li><a href="#">Services</a></li>
-            <li><a href="#">Contact</a></li>
-          </ul>
-        </nav>
-      </div>
+    <footer className="bg-dark text-light py-4">
+      <Container>
+        <Row className="mb-3">
+          
+          <Col md={3}>
+            <img src={logo} alt="Company Logo" className="img-fluid mb-3" style={{maxWidth: '150px'}} />
+            <p>© 2024 Your Company Name. All rights reserved.</p>
+          </Col>
+
+          <Col md={3}>
+            <h5>Quick Links</h5>
+            <Nav className="flex-column">
+              <Nav.Item><Nav.Link href="/" className="text-light">Home</Nav.Link></Nav.Item>
+              <Nav.Item><Nav.Link href="/about" className="text-light">About</Nav.Link></Nav.Item>
+              <Nav.Item><Nav.Link href="/services" className="text-light">Services</Nav.Link></Nav.Item>
+              <Nav.Item><Nav.Link href="/contact" className="text-light">Contact</Nav.Link></Nav.Item>
+            </Nav>
+          </Col>
+
+          <Col md={3}>
+            <h5>Follow Us</h5>
+            <div className="d-flex">
+              <a href="https://www.facebook.com" className="text-light me-3"><FaFacebook size={24} /></a>
+              <a href="https://www.twitter.com" className="text-light me-3"><FaTwitter size={24} /></a>
+              <a href="https://www.instagram.com" className="text-light me-3"><FaInstagram size={24} /></a>
+              <a href="https://www.linkedin.com" className="text-light"><FaLinkedin size={24} /></a>
+            </div>
+          </Col>
+
+          <Col md={3}>
+            <h5>Download Our App</h5>
+            <div className="d-flex">
+              <a href="#" className="text-light me-3"><FaApple size={24} /></a>
+              <a href="#" className="text-light"><FaAndroid size={24} /></a>
+            </div>
+          </Col>
+        </Row>
+      </Container>
     </footer>
   );
 };
