d884193741e9cc89c114e081e9a23fb353ea511a
joshy Lucas <96329740+joshhy12@users.noreply.github.com>
2024-09-25 11:47:34 +0300

    init projet

---
+++ src/Footer.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="footer-content">
+        <p>&copy; 2023 Your Company Name. All rights reserved.</p>
+        <nav>
+          <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Services</a></li>
+            <li><a href="#">Contact</a></li>
+          </ul>
+        </nav>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
