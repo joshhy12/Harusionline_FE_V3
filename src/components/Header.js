8dcfed05cf3c2c333c61218a09d05778c8458889
joshy Lucas <96329740+joshhy12@users.noreply.github.com>
2024-09-30 15:21:17 +0300

    Updated Header component, removed app icons, and modified Footer and Header CSS styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,29 +1,28 @@
 import React from 'react';
 import { Navbar, Nav, Container, Button } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
-import '../styles/Header.css'; 
+import { Link, NavLink } from 'react-router-dom';
+import '../styles/Header.css';
 
 function Header() {
   return (
-<Navbar bg="white" variant="light" expand="lg" sticky="top" className="py-3 custom-shadow">
-<Container>
+    <Navbar bg="white" variant="light" expand="lg" sticky="top" className="py-3 custom-shadow">
+      <Container>
         <Navbar.Brand as={Link} to="/Home">
           <img
             src="/logo1.png"
             width="30"
             height="30"
             className="d-inline-block align-top me-2"
-            alt="My React App Logo"
-          />
-          My React App
+            alt="Harusi Online"
+          /> Harusi Online
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link as={Link} to="/Home" className="px-3">Home</Nav.Link>
-            <Nav.Link as={Link} to="/About" className="px-3">About</Nav.Link>
-            <Nav.Link as={Link} to="/Contact" className="px-3">Contact</Nav.Link>
-            <Nav.Link as={Link} to="/e_card" className="px-3">E-Card</Nav.Link>
+            <Nav.Link as={NavLink} to="/Home" className="px-3" activeClassName="active-link">Home</Nav.Link>
+            <Nav.Link as={NavLink} to="/About" className="px-3" activeClassName="active-link">About</Nav.Link>
+            <Nav.Link as={NavLink} to="/Contact" className="px-3" activeClassName="active-link">Contact</Nav.Link>
+            <Nav.Link as={NavLink} to="/e_card" className="px-3" activeClassName="active-link">E-Card</Nav.Link>
           </Nav>
           <Nav className="ms-auto">
             <Button variant="outline-primary" className="me-2">Login</Button>
