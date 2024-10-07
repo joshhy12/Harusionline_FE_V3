44a5913ddca995e0a5aff739599527a230e86525
joshy Lucas <96329740+joshhy12@users.noreply.github.com>
2024-10-07 12:47:46 +0300

    Updated favicon, title, and routing in index.html, removed unnecessary code in App.js, and changed active link color in Header.css

--- src/App.js
+++ src/App.js
@@ -3,13 +3,11 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
-
 import Home from './pages/Home';
 import About from './pages/About';
 import Contact from './pages/Contact';
-import  E_card from './pages/E_card';
+import E_card from './pages/E_card';
 
-   
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
@@ -18,22 +16,18 @@ function App() {
       <div className="App">
        
         <Header />
-
-
         <main>
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/home" element={<Home />} />
             <Route path="/about" element={<About />} />
             <Route path="/contact" element={<Contact />} />
-            <Route path="/e_card" element={<E_card />} />
+            <Route path="/e_card" element={<E_card/>} />
           </Routes>
         </main>
 
-
         <Footer />
 
-        
       </div>
     </Router>
   );
