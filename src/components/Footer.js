aeea7bbf3a6e6e923ca6232ceb2a27e7796b4f01
joshy Lucas <96329740+joshhy12@users.noreply.github.com>
2024-10-09 16:40:51 +0300

    Updated Footer component with new layout, social media links, and styles

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,63 +1,64 @@
 import React from 'react';
 import logo from '../images/logo.png';
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container, Row, Col, Nav } from 'react-bootstrap';
 import { FaFacebook, FaTwitter, FaInstagram, FaLinkedin, FaWhatsapp } from 'react-icons/fa';
 import '../styles/Footer.css';
+import { Link } from 'react-router-dom';
+
 
 const Footer = () => {
   const currentYear = new Date().getFullYear();
 
   return (
+
     <footer id="footer">
       <div className="footer-top">
         <Container>
           <Row>
             <Col lg={3} md={6} className="footer-info">
+              <h3>Harusi Online..</h3>
 
-
-          <div style={{ textAlign: 'center' }}>
-  <h3 style={{ marginBottom: '15px', color: 'black' }}>Harusi online Tanzania</h3>
-  <img src={logo} alt="Company Logo" className="img-fluid mb-3" style={{ maxWidth: '150px' }} />
-  <p style={{ color: 'black' }}>
-    A108 Adam Street <br />
-    NY 535022, USA<br />
-    <strong>Phone:</strong> +1 5589 55488 55<br />
-    <strong>Email:</strong> info@example.com<br />
-  </p>
-</div>
+              <p style={{ color: 'black' }}>
+                New Safari Hotel Business Center  <br />
+                Arusha, Tanzania<br />
+                <strong>Phone:</strong> <a href="tel:+255747636516">+255 747 636 516</a><br />
+                <strong>Email:</strong> <a href="mailto:info@harusionline.com">info@harusionline.com</a><br />
+              </p>
 
 
               <div className="social-links mt-3">
-                <a href="#" className="btn btn-outline-primary me-2"><FaTwitter /></a>
-                <a href="#" className="btn btn-outline-primary me-2"><FaFacebook /></a>
-                <a href="#" className="btn btn-outline-primary me-2"><FaInstagram /></a>
-                <a href="#" className="btn btn-outline-primary me-2"><FaWhatsapp /></a>
-                <a href="#" className="btn btn-outline-primary me-2"><FaLinkedin /></a>
+                <Link to="#" className="btn btn-outline-primary me-2"><i className="bi bi-twitter"></i></Link>
+                <Link to="https://www.facebook.com/harusionlinetz/" className="btn btn-outline-primary me-2"  target="_blank" ><i className="bi bi-facebook"></i></Link>
+                <Link to="https://www.instagram.com/harusionline/" className="btn btn-outline-primary me-2"  target="_blank"  ><i className="bi bi-instagram"></i></Link>
+                <a href="https://wa.me/255747636516" className="btn btn-outline-primary me-2" target="_blank" rel="noopener noreferrer"> <i className="bi bi-whatsapp"></i>
+                </a>
               </div>
+
+
             </Col>
             <Col lg={2} md={6} className="footer-links">
               <h4>Useful Links</h4>
               <ul>
-                <li><i className="bx bx-chevron-right"></i> <a href="#">Home</a></li>
-                <li><i className="bx bx-chevron-right"></i> <a href="#">About us</a></li>
-                <li><i className="bx bx-chevron-right"></i> <a href="#">Services</a></li>
-                <li><i className="bx bx-chevron-right"></i> <a href="#">Terms of service</a></li>
-                <li><i className="bx bx-chevron-right"></i> <a href="#">Privacy policy</a></li>
+                <li><i className="bi bi-chevron-right"></i> <Link to="/">Home</Link></li>
+                <li><i className="bi bi-chevron-right"></i> <Link to="/about">About us</Link></li>
+                <li><i className="bi bi-chevron-right"></i> <Link to="#services">Services</Link></li>
+                <li><i className="bi bi-chevron-right"></i> <Link to="/e_card">E-Card</Link></li>
+                <li><i className="bi bi-chevron-right"></i> <Link to="">Jisajili</Link></li>
               </ul>
             </Col>
             <Col lg={3} md={6} className="footer-links">
               <h4>Our Services</h4>
               <ul>
-                <li><i className="bx bx-chevron-right"></i> <a href="#">Web Design</a></li>
-                <li><i className="bx bx-chevron-right"></i> <a href="#">Web Development</a></li>
-                <li><i className="bx bx-chevron-right"></i> <a href="#">Product Management</a></li>
-                <li><i className="bx bx-chevron-right"></i> <a href="#">Marketing</a></li>
-                <li><i className="bx bx-chevron-right"></i> <a href="#">Graphic Design</a></li>
+                <li><i className="bi bi-chevron-right"></i> <Link to="#tuma-taarifa">Tuma Taarifa</Link></li>
+                <li><i className="bi bi-chevron-right"></i> <Link to="#kumbusha-michango">Kumbusha Michango</Link></li>
+                <li><i className="bx bi-chevron-right"></i> <Link to="#kadi-za-kidigitali">Kadi za Kidigitali</Link></li>
+                <li><i className="bx bi-chevron-right"></i> <Link to="#tuma-mialiko">Tuma Mialiko</Link></li>
+                <li><i className="bx bi-chevron-right"></i> <Link to="#ripori-ya-fedha">Ripori ya Fedha</Link></li>
               </ul>
             </Col>
             <Col lg={4} md={6} className="footer-newsletter">
               <h4>Our Newsletter</h4>
-              <p>Tamen quem nulla quae legam multos aute sint culpa legam noster magna</p>
+              <p>Join our community of engaged couples and receive valuable insights directly to your inbox.</p>
               <form action="" method="post">
                 <input type="email" name="email" />
                 <input type="submit" value="Subscribe" />
@@ -65,14 +66,15 @@ const Footer = () => {
 
               <h5 className="mt-4">Download Our App</h5>
               <div className="d-flex">
-                <a href="https://www.apple.com" className="me-3" target="_blank" rel="noopener noreferrer">
+                <a href="https://apps.apple.com/us/app/harusi-online/id1629222934" className="me-3" target="_blank" rel="noopener noreferrer">
                   <img src={require('../images/apple.png')} alt="Apple App Store" style={{ width: '124px', height: '39px' }} />
                 </a>
-                <a href="https://play.google.com" target="_blank" rel="noopener noreferrer">
+                <a href="https://play.google.com/store/apps/details?id=harusi.online.mobile&pcampaignid=web_share" target="_blank" rel="noopener noreferrer">
                   <img src={require('../images/android.png')} alt="Google Play Store" style={{ width: '124px', height: '39px' }} />
                 </a>
               </div>
             </Col>
+
           </Row>
         </Container>
       </div>
